285743757b53443732d25ce95b6dce81965f5457
mac <mac@Cloudy.local>
2023-05-29 18:22:47 -0700

    database

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
     client: 'pg',
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
+    connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
     }
 });
 
